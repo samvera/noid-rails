8ddcfbccbf765539233d0501de23292576dc30c6
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2015-03-30 09:36:15 -0700

    Module should require its components so downstream adopters can use them.

--- lib/active_fedora/noid.rb
+++ lib/active_fedora/noid.rb
@@ -1,5 +1,7 @@
 require 'active_fedora/noid/version'
 require 'active_fedora/noid/config'
+require 'active_fedora/noid/service'
+require 'active_fedora/noid/synchronized_minter'
 
 module ActiveFedora
   module Noid

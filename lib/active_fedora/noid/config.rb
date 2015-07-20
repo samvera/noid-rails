9561481797d7f9230f30d5ac0a0e8ee34103d7d6
Esmé Cowles <escowles@ucsd.edu>
2015-07-20 16:32:07 -0400

    Calculating baseurl length instead of assumig based on default settings, adding tests for templates of varying lengths

--- lib/active_fedora/noid/config.rb
+++ lib/active_fedora/noid/config.rb
@@ -21,13 +21,24 @@ module ActiveFedora
       end
 
       def translate_uri_to_id
-        lambda { |uri| URI(uri).path.split('/', 9).last }
+        lambda { |uri| URI(uri).path.split('/', baseparts).last }
       end
 
       def translate_id_to_uri
         lambda do |id|
-          "#{ActiveFedora.fedora.host}#{ActiveFedora.fedora.base_path}/#{ActiveFedora::Noid.treeify(id)}"
+          "#{baseurl}/#{ActiveFedora::Noid.treeify(id)}"
         end
+      end
+
+      private
+
+      def baseurl
+        "#{ActiveFedora.fedora.host}#{ActiveFedora.fedora.base_path}"
+      end
+
+      def baseparts
+        treeparts = [(template.gsub(/\.[rsz]/,'').length.to_f/2).ceil, 4].min
+        baseurl.count('/') + treeparts
       end
     end
   end

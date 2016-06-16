b8db112ed652630df6485ed4d906252493f95a76
Joe Atzberger <ohiocore@gmail.com>
2016-06-16 16:46:52 -0700

    Refactor tests for sync minter
    - Avoid allow_any_instance_of
    - Stop testing ActiveFedora::Noid::Service here
    - factor out common cases (like Minter::Db spec)
    
    Possible TODO: test both minters w/ shared examples, since they are
    almost identical.

--- spec/unit/synchronized_minter_spec.rb
+++ spec/unit/synchronized_minter_spec.rb
@@ -1,10 +1,16 @@
 describe ActiveFedora::Noid::SynchronizedMinter do
-  it { is_expected.to respond_to(:mint) }
+  before :each do
+    # default novel mintings
+    allow(ActiveFedora::Base).to receive(:exists?).and_return(false)
+    allow(ActiveFedora::Base).to receive(:gone?).and_return(false)
+  end
 
+  let(:minter) { described_class.new }
+
+  it { is_expected.to respond_to(:mint) }
   it 'has a default statefile' do
     expect(subject.statefile).to eq ActiveFedora::Noid.config.statefile
   end
-
   it 'has a default template' do
     expect(subject.template.to_s).to eq ActiveFedora::Noid.config.template
   end
@@ -13,65 +19,52 @@ describe ActiveFedora::Noid::SynchronizedMinter do
     let(:template) { '.rededk' }
     let(:statefile) { '/tmp/foobar' }
 
-    subject { ActiveFedora::Noid::SynchronizedMinter.new(template, statefile) }
+    subject { described_class.new(template, statefile) }
 
     it 'respects the custom template' do
       expect(subject.template.to_s).to eq template
     end
-
     it 'respects the custom statefile' do
       expect(subject.statefile).to eq statefile
     end
   end
 
   describe '#mint' do
-    before do
-      allow(ActiveFedora::Base).to receive(:exists?).and_return(false)
-      allow(ActiveFedora::Base).to receive(:gone?).and_return(false)
-    end
-
-    subject { ActiveFedora::Noid::Service.new.mint }
-
+    subject { minter.mint }
     it { is_expected.not_to be_empty }
-
     it 'does not mint the same ID twice in a row' do
-      expect(subject).not_to eq ActiveFedora::Noid::Service.new.mint
+      expect(subject).not_to eq described_class.new.mint
     end
-
     it 'is valid' do
-      expect(ActiveFedora::Noid::Service.new.valid?(subject)).to be true
+      expect(minter.valid?(subject)).to be true
+      expect(described_class.new.valid?(subject)).to be true
     end
   end
 
-  context "when the pid already exists in Fedora" do
+  context 'conflicts' do
+    let(:unique_pid) { 'bb22bb22b' }
     let(:existing_pid) { 'ef12ef12f' }
-    let(:unique_pid) { 'bb22bb22b' }
-
-    before do
-      allow_any_instance_of(ActiveFedora::Noid::SynchronizedMinter).to receive(:next_id).and_return(existing_pid, unique_pid)
-      allow(ActiveFedora::Base).to receive(:exists?).with(existing_pid).and_return(true)
-      allow(ActiveFedora::Base).to receive(:exists?).with(unique_pid).and_return(false)
-      allow(ActiveFedora::Base).to receive(:gone?).and_return(false)
+    before :each do
+      expect(minter).to receive(:next_id).and_return(existing_pid, unique_pid)
     end
 
-    it 'skips the existing pid' do
-      expect(subject.mint).to eq unique_pid
-    end
-  end
-
-  context "when the pid already existed in Fedora and now is gone" do
-    let(:gone_pid) { 'ef12ef12f' }
-    let(:unique_pid) { 'bb22bb22b' }
-
-    before do
-      allow_any_instance_of(ActiveFedora::Noid::SynchronizedMinter).to receive(:next_id).and_return(gone_pid, unique_pid)
-      allow(ActiveFedora::Base).to receive(:gone?).with(gone_pid).and_return(true)
-      allow(ActiveFedora::Base).to receive(:gone?).with(unique_pid).and_return(false)
-      allow(ActiveFedora::Base).to receive(:exists?).and_return(false)
+    context 'when the pid already exists in Fedora' do
+      before do
+        expect(ActiveFedora::Base).to receive(:exists?).with(existing_pid).and_return(true)
+      end
+      it 'skips the existing pid' do
+        expect(minter.mint).to eq unique_pid
+      end
     end
 
-    it 'skips the deleted pid' do
-      expect(subject.mint).to eq unique_pid
+    context 'when the pid already existed in Fedora and now is gone' do
+      let(:gone_pid) { existing_pid }
+      before do
+        expect(ActiveFedora::Base).to receive(:gone?).with(gone_pid).and_return(true)
+      end
+      it 'skips the deleted pid' do
+        expect(minter.mint).to eq unique_pid
+      end
     end
   end
 end

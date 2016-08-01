3234a2bd29bdd9df3c829c6d325549d4ff8ca6c9
Michael J. Giarlo <mjgiarlo@stanford.edu>
2016-08-01 14:06:07 -0700

    Specify global namespace for `File` so that other modules don't trip us up. Refs #30

--- lib/generators/active_fedora/noid/install_generator.rb
+++ lib/generators/active_fedora/noid/install_generator.rb
@@ -1,7 +1,7 @@
 module ActiveFedora
   module Noid
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path('../templates', __FILE__)
+      source_root ::File.expand_path('../templates', __FILE__)
 
       desc <<-END_OF_DESC
 Copies DB migrations

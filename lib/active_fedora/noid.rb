d1a863f1cb13ca2b4b3be826dba64f780d9c02f4
Michael J. Giarlo <mjgiarlo@stanford.edu>
2016-10-26 09:34:31 -0700

    Treeify algorithm should raise ArgumentError when passed nil or empty string

--- lib/active_fedora/noid.rb
+++ lib/active_fedora/noid.rb
@@ -15,6 +15,7 @@ module ActiveFedora
       end
 
       def treeify(identifier)
+        raise ArgumentError, 'Identifier must be a string of size > 0 in order to be treeified' if identifier.blank?
         head = identifier.split('/').first
         head.gsub!(/#.*/, '')
         (head.scan(/..?/).first(4) + [identifier]).join('/')

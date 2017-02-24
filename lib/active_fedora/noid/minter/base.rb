f23f3abacc76036da91e1b11d489f9894ddb2faf
Joe Atzberger <ohiocore@gmail.com>
2017-02-23 19:08:20 -0800

    tweak YARD on Base

--- lib/active_fedora/noid/minter/base.rb
+++ lib/active_fedora/noid/minter/base.rb
@@ -8,15 +8,13 @@ module ActiveFedora
       class Base < ::Noid::Minter
         ##
         # @param template [#to_s] a NOID template
-        #
         # @see Noid::Template
         def initialize(template = default_template)
           super(template: template.to_s)
         end
 
         ##
-        # Sychronously mint a new identifier. Guarantees the ID is not
-        # already reserved in ActiveFedora.
+        # Sychronously mint a new identifier. Guarantees the ID is not already reserved in ActiveFedora.
         #
         # @return [String] the minted identifier
         def mint
@@ -29,7 +27,7 @@ module ActiveFedora
         end
 
         ##
-        # @return [Hash] an object representing the current minter state
+        # @return [Hash{Symbol => String, Object}] representation of the current minter state
         def read
           raise NotImplementedError, 'Implement #read in child class'
         end
@@ -38,7 +36,6 @@ module ActiveFedora
         # Updates the minter state to that of the `minter` parameter.
         #
         # @param minter [Minter::Base]
-        #
         # @return [void]
         def write!(_)
           raise NotImplementedError, 'Implement #write! in child class'
@@ -53,7 +50,7 @@ module ActiveFedora
         end
 
         ##
-        # @return [String] a new identifier.
+        # @return [String] a new identifier
         def next_id
           raise NotImplementedError, 'Implement #next_id in child class'
         end
